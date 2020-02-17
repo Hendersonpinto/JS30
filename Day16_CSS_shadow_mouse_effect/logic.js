bbfc18a8e0df48ed0759b7b5a21f187cd7a693c9
Henderson Pinto <hendersoncode@gmail.com>
2020-02-17 14:57:11 +0100

    Day16

--- Day16_CSS_shadow_mouse_effect/logic.js
+++ Day16_CSS_shadow_mouse_effect/logic.js
@@ -1,7 +1,7 @@
 const hero = document.querySelector('.hero')
 const text = hero.querySelector('h1')
 console.log(hero)
-const walk = 50
+const walk = 200
 
 function textEffect(event) {
     const {
@@ -12,10 +12,10 @@ function textEffect(event) {
     const xTransformed = (x / hero.offsetWidth * walk) - (walk / 2);
     const yTransformed = (y / hero.offsetHeight * walk) - (walk / 2);
     text.style.textShadow = `
-    ${xTransformed}px ${yTransformed}px 0 rgba(0, 0, 0, 1),
-    ${xTransformed}px ${yTransformed * -1}px 0 rgba(255, 0, 0, 1),
-    ${xTransformed * -1}px ${yTransformed * -1}px 0 rgba(0, 255, 0, 1),
-    ${xTransformed * -1}px ${yTransformed}px 0 rgba(0, 0, 255, 1)`;
+    ${xTransformed}px ${yTransformed}px 0 rgba(0, 0, 0, 0.4),
+    ${xTransformed}px ${yTransformed * -1}px 0 rgba(255, 0, 0, 0.4),
+    ${xTransformed * -1}px ${yTransformed * -1}px 0 rgba(0, 255, 0, 0.2),
+    ${xTransformed * -1}px ${yTransformed}px 0 rgba(0, 0, 255, 0.4)`;
 
 }
 
