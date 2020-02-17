9b5e23098eaaf0f675af95bda3718b9d304ef302
Henderson Pinto <hendersoncode@gmail.com>
2020-02-17 13:27:48 +0100

    Day15

--- Day15_event_delegation/logic.js
+++ Day15_event_delegation/logic.js
@@ -1,8 +1,6 @@
 const form = document.querySelector('.add-items');
 const itemsList = document.querySelector('.plates');
-const items = [];
-
-
+const items = JSON.parse(localStorage.getItem('items')) || [];
 
 function addItem(event) {
     event.preventDefault();
@@ -10,11 +8,53 @@ function addItem(event) {
     const item = {
         itemValue,
         done: false
-
     };
+    items.push(item);
     this.reset();
-    console.log(item);
+    populateList(items, itemsList);
+    localStorage.setItem('items', JSON.stringify(items));
+}
+
+function addItem2(event) {
+    event.preventDefault();
+    const name = this.querySelector('[name=item').value;
+    if (itemsList.firstElementChild.innerText === 'Loading Tapas...') {
+        itemsList.innerHTML = '';
+    }
+    itemsList.insertAdjacentHTML(
+        'beforeend',
+        `<li>
+    <label for="">${name}</label>
+    </li>`
+    );
+}
+
+function populateList(plates = [], platesList) {
+    console.log('this ran')
+    platesList.innerHTML = plates
+        .map((plate, i) => {
+            console.log(plate);
+            return `
+            <li>
+              <input type="checkbox" data-index=${i} id="item${i}" ${plate.done ? 'checked' : ''} />
+              <label for="item${i}">${plate.itemValue}</label>
+            </li>
+          `;
+        })
+        .join('');
+}
+
+function toggleCheck(event) {
+    if (!event.target.matches('input')) return;
+    console.log(event)
+    const index = event.target.dataset.index;
+    items[index].done = !items[index].done;
+    localStorage.setItem('items', JSON.stringify(items));
+    populateList(items, itemsList)
 }
 
 
+
 form.addEventListener('submit', addItem);
+itemsList.addEventListener(('click'), toggleCheck)
+populateList(items, itemsList)
